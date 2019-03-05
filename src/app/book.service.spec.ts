cd9be80b5b70bef53a142e0192aa67dc2e9d845b
Matthew Kelly <coolbeandroid@gmail.com>
2019-03-05 14:00:08 +1100

    Update unit test to use first operator

--- src/app/book.service.spec.ts
+++ src/app/book.service.spec.ts
@@ -1,7 +1,9 @@
 import { TestBed } from '@angular/core/testing';
+import { first } from 'rxjs/operators';
 
 import { BookService } from './book.service';
 import { BookCategoriesEnum } from './constants/constants';
+import { Book } from 'src/types/book';
 
 describe('BookServiceService', () => {
   beforeEach(() => TestBed.configureTestingModule({}));
@@ -16,16 +18,19 @@ describe('BookServiceService', () => {
     const book1 = { 'title': 'Title', 'description': 'Book one', 'category': BookCategoriesEnum.Comedy};
     const book2 = { 'title': 'Title', 'description': 'Book two', 'category': BookCategoriesEnum.Comedy};
     service.addBook(book1);
-    const sub1 = service.books$.subscribe((books) => {
-      expect(books.length).toEqual(1);
-      expect(books[0]).toEqual(book1);
-    });
-    sub1.unsubscribe();
+    service.books$
+      .pipe(first())
+      .subscribe((books: Array<Book>) => {
+        expect(books.length).toEqual(1);
+        expect(books[0]).toEqual(book1);
+      });
     service.addBook(book2);
-    service.books$.subscribe((books) => {
-      expect(books.length).toEqual(2);
-      expect(books[0]).toEqual(book1);
-      expect(books[1]).toEqual(book2);
-    });
+    service.books$
+      .pipe(first())
+      .subscribe((books: Array<Book>) => {
+        expect(books.length).toEqual(2);
+        expect(books[0]).toEqual(book1);
+        expect(books[1]).toEqual(book2);
+      });
   });
 });
